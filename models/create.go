adb443e9f01008969f074440fc20e2d90392753f
Bityacht-Developer <developer@bityacht.io>
2024-02-16 15:28:46 +0800

    add a comment
    add a comment

--- models/create.go
+++ models/create.go
@@ -1,6 +1,6 @@
 package model
 func CreateTodo() error{
-	insertQ, err:=con.Query("INSERT INTO TODO VALUES($1,$2)","Daniel","Work1")
+	insertQ, err:=con.Query("INSERT INTO TODO VALUES($1,$2)","Daniel","Work1")//insertQ->insert query
 defer insertQ.Close()
 if err !=nil{return err}
 return nil
