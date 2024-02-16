5e7e0bc9db0355ff48971dd1c0fa0153b223e9a3
Bityacht-Developer <developer@bityacht.io>
2024-02-16 17:42:02 +0800

    db insertion complete
    db insertion complete

--- controller/create.go
+++ controller/create.go
@@ -1,18 +1,25 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func create() http.HandlerFunc{
 return func(w http.ResponseWriter, r *http.Request){
-	if r.Method==http.MethodGet{
+	if r.Method==http.MethodPost{
 //1.take some data
+data :=view.PostRequest{}
+json.NewEncoder(r.Body).Decode(&data)
 //2.save it in mySQL
-if err :=model.CreateTodo();err!=nil{
+
+//
+if err :=model.CreateTodo(data.name,data.todo);err!=nil{
 	w.Write([]byte("Some error"))
 	return
 }
+w.WriteHeader(http.StatusCreated)
+json.NewEncoder(w).Encode(data)
 
 	}
 }
--- models/create.go
+++ models/create.go
@@ -1,6 +1,6 @@
 package model
-func CreateTodo() error{
-	insertQ, err:=con.Query("INSERT INTO TODO VALUES($1,$2)","Daniel","Work1")//insertQ->insert query
+func CreateTodo(name,todo string) error{
+	insertQ, err:=con.Query("INSERT INTO TODO VALUES(?,?)",name,todo)//insertQ->insert query
 defer insertQ.Close()
 if err !=nil{return err}
 return nil
